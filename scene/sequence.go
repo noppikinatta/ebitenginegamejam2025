151c6a99d52e707de11d260b8009da8e54c4ef83
noppikinatta <noppikitouroku@gmail.com>
2025-06-28 22:42:09 +0900

    add lang switching

--- scene/sequence.go
+++ scene/sequence.go
@@ -10,7 +10,6 @@ import (
 )
 
 func CreateSequence(input *ui.Input) ebiten.Game {
-
 	title := NewTitle(input)
 	inGame := NewInGame(input)
 	result := NewResult(input)
@@ -20,5 +19,27 @@ func CreateSequence(input *ui.Input) ebiten.Game {
 	title.Init(inGame, seq, tran)
 	inGame.Init(result, seq, tran)
 
-	return seq
+	return &wrapperGame{
+		langSwitcher: &langSwitcher{},
+		game:         seq,
+	}
+}
+
+type wrapperGame struct {
+	langSwitcher *langSwitcher
+	game         ebiten.Game
+}
+
+func (w *wrapperGame) Update() error {
+	w.langSwitcher.Update()
+	return w.game.Update()
+}
+
+func (w *wrapperGame) Draw(screen *ebiten.Image) {
+	w.game.Draw(screen)
+	w.langSwitcher.Draw(screen)
+}
+
+func (w *wrapperGame) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return w.game.Layout(outsideWidth, outsideHeight)
 }
