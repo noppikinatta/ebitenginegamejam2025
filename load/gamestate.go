8c3ffa549534a05fbdce90e9deadaeb33633544c
noppikinatta <noppikitouroku@gmail.com>
2025-06-28 18:30:22 +0900

    add nations WIP

--- load/gamestate.go
+++ load/gamestate.go
@@ -26,20 +26,6 @@ func createMyNation() *core.MyNation {
 	return &core.MyNation{
 		BaseNation: core.BaseNation{
 			NationID: "player",
-			Market: &core.Market{
-				Level: 1.0,
-				Items: []*core.MarketItem{
-					{
-						CardPack: &core.CardPack{
-							Ratios: map[core.CardID]int{
-								"battle_card_1": 1,
-							},
-							NumPerOpen: 2,
-						},
-						Price: core.ResourceQuantity{Money: 0},
-					},
-				},
-			},
 		},
 		BasicYield: core.ResourceQuantity{
 			Food:  1,
@@ -63,44 +49,319 @@ func createMapGrid(myNation *core.MyNation) *core.MapGrid {
 	size := core.MapGridSize{X: 5, Y: 5}
 	points := make([]core.Point, size.Length())
 
-	for i := range points {
-		x, y := size.XY(i)
+	cardPacks := map[string]*core.CardPack{
+		"cardpack-free": {
+			NumPerOpen: 1,
+			Ratios: map[core.CardID]int{
+				"battlecard-soldier": 10,
+				"battlecard-knight":  1,
+			},
+		},
+		"cardpack-soldiers": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-soldier": 5,
+				"battlecard-archer":  5,
+				"battlecard-knight":  1,
+			},
+		},
+		"cardpack-knights": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-knight":      3,
+				"battlecard-general":     1,
+				"structurecard-catapult": 2,
+			},
+		},
+		"cardpack-politics": {
+			NumPerOpen: 2,
+			Ratios: map[core.CardID]int{
+				"structurecard-farm":       1,
+				"structurecard-woodcutter": 1,
+				"structurecard-tunnel":     1,
+				"structurecard-market":     1,
+			},
+		},
+		"cardpack-war": {
+			NumPerOpen: 5,
+			Ratios: map[core.CardID]int{
+				"structurecard-catapult": 2,
+				"structurecard-ballista": 1,
+				"structurecard-camp":     1,
+			},
+		},
+		"cardpack-magic": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-wizard":       5,
+				"structurecard-mana-node": 5,
+				"battlecard-mage":         1,
+			},
+		},
+		"cardpack-mystic": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-fortune":      2,
+				"structurecard-mana-node": 2,
+				"structurecard-temple":    2,
+				"battlecard-mage":         1,
+			},
+		},
+		"cardpack-mineral": {
+			NumPerOpen: 2,
+			Ratios: map[core.CardID]int{
+				"structurecard-tunnel":  4,
+				"structurecard-smelter": 1,
+				"battlecard-blacksmith": 4,
+			},
+		},
+		"cardpack-mechanical": {
+			NumPerOpen: 2,
+			Ratios: map[core.CardID]int{
+				"battlecard-golem":       1,
+				"structurecard-smelter":  2,
+				"battlecard-artillery":   2,
+				"structurecard-ballista": 1,
+			},
+		},
+		"cardpack-fancy": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-clown":    5,
+				"battlecard-wrestler": 2,
+				"battlecard-bard":     5,
+				"battlecard-fortune":  1,
+			},
+		},
+		"cardpack-samurai": {
+			NumPerOpen: 2,
+			Ratios: map[core.CardID]int{
+				"battlecard-samurai": 4,
+				"battlecard-ninja":   2,
+				"battlecard-monk":    3,
+				"structurecard-camp": 1,
+			},
+		},
+		"cardpack-siege": {
+			NumPerOpen: 2,
+			Ratios: map[core.CardID]int{
+				"structurecard-catapult":     2,
+				"structurecard-ballista":     2,
+				"structurecard-orban-cannon": 1,
+				"battlecard-artillery":       2,
+			},
+		},
+		"cardpack-finance": {
+			NumPerOpen: 2,
+			Ratios: map[core.CardID]int{
+				"battlecard-blacksmith": 2,
+				"structurecard-market":  2,
+				"structurecard-mint":    1,
+			},
+		},
+		"cardpack-building": {
+			NumPerOpen: 2,
+			Ratios: map[core.CardID]int{
+				"structurecard-granary": 1,
+				"structurecard-sawmill": 1,
+				"structurecard-smelter": 1,
+				"structurecard-mint":    1,
+				"structurecard-temple":  1,
+				"structurecard-camp":    1,
+			},
+		},
+		"cardpack-forest": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-archer":        2,
+				"structurecard-farm":       1,
+				"structurecard-woodcutter": 2,
+				"structurecard-mana-node":  1,
+			},
+		},
+		"cardpack-desert": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-fortune":      2,
+				"battlecard-bard":         1,
+				"structurecard-market":    2,
+				"structurecard-mana-node": 1,
+			},
+		},
+		"cardpack-mountain": {
+			NumPerOpen: 3,
+			Ratios: map[core.CardID]int{
+				"battlecard-blacksmith":    2,
+				"structurecard-tunnel":     1,
+				"structurecard-woodcutter": 2,
+				"battlecard-soldier":       1,
+			},
+		},
+	}
+	cardPackPrices := map[string]core.ResourceQuantity{
+		"cardpack-free":       {},
+		"cardpack-soldiers":   {Food: 2, Money: 2},
+		"cardpack-knights":    {Food: 5, Iron: 5, Money: 10},
+		"cardpack-politics":   {Wood: 5},
+		"cardpack-war":        {Iron: 20, Wood: 20, Money: 20},
+		"cardpack-magic":      {Mana: 5, Food: 5},
+		"cardpack-mystic":     {Mana: 20},
+		"cardpack-mineral":    {Wood: 10},
+		"cardpack-mechanical": {Iron: 30},
+		"cardpack-fancy":      {Money: 50, Food: 10},
+		"cardpack-samurai":    {Iron: 50, Food: 10},
+		"cardpack-siege":      {Iron: 50, Wood: 50},
+		"cardpack-finance":    {Money: 30, Wood: 10},
+		"cardpack-building":   {Money: 30, Wood: 30},
+		"cardpack-forest":     {Food: 5, Wood: 5},
+		"cardpack-desert":     {Money: 10, Food: 10},
+		"cardpack-mountain":   {Food: 5, Wood: 5},
+	}
 
-		if x == 0 && y == 0 {
-			points[i] = &core.MyNationPoint{MyNation: myNation}
+	myNation.Market = &core.Market{
+		Level: 1.0,
+		Items: []*core.MarketItem{
+			{
+				CardPack: cardPacks["cardpack-free"],
+				Price:    cardPackPrices["cardpack-free"],
+			},
+			{
+				CardPack: cardPacks["cardpack-soldiers"],
+				Price:    cardPackPrices["cardpack-soldiers"],
+			},
+		},
+	}
+	points[size.Index(0, 0)] = &core.MyNationPoint{MyNation: myNation}
+
+	// 各国
+	points[size.Index(0, 2)] = &core.OtherNationPoint{ // 森の国
+		OtherNation: &core.OtherNation{
+			BaseNation: core.BaseNation{
+				NationID: "nation-forest",
+				Market: &core.Market{
+					Level: 1.0,
+					Items: []*core.MarketItem{
+						{
+							CardPack: cardPacks["cardpack-forest"],
+							Price:    cardPackPrices["cardpack-forest"],
+						},
+					},
+				},
+			},
+		},
+	}
+	points[size.Index(2, 0)] = &core.OtherNationPoint{ // 山の国
+		OtherNation: &core.OtherNation{
+			BaseNation: core.BaseNation{
+				NationID: "nation-mountain",
+				Market: &core.Market{
+					Level: 1.0,
+					Items: []*core.MarketItem{
+						{
+							CardPack: cardPacks["cardpack-mountain"],
+							Price:    cardPackPrices["cardpack-mountain"],
+						},
+					},
+				},
+			},
+		},
+	}
+	points[size.Index(2, 2)] = &core.OtherNationPoint{ // 砂漠の国
+		OtherNation: &core.OtherNation{
+			BaseNation: core.BaseNation{
+				NationID: "nation-desert",
+				Market: &core.Market{
+					Level: 1.0,
+					Items: []*core.MarketItem{
+						{
+							CardPack: cardPacks["cardpack-desert"],
+							Price:    cardPackPrices["cardpack-desert"],
+						},
+						{
+							CardPack: cardPacks["cardpack-politics"],
+							Price:    cardPackPrices["cardpack-politics"],
+						},
+					},
+				},
+			},
+		},
+	}
+	points[size.Index(0, 4)] = &core.OtherNationPoint{ // 侍の国
+		OtherNation: &core.OtherNation{
+			BaseNation: core.BaseNation{
+				NationID: "nation-samurai",
+				Market: &core.Market{
+					Level: 1.0,
+					Items: []*core.MarketItem{
+						{
+							CardPack: cardPacks["cardpack-samurai"],
+							Price:    cardPackPrices["cardpack-samurai"],
+						},
+					},
+				},
+			},
+		},
+	}
+	points[size.Index(4, 0)] = &core.OtherNationPoint{ // 魔法の国
+		OtherNation: &core.OtherNation{
+			BaseNation: core.BaseNation{
+				NationID: "nation-magical",
+				Market: &core.Market{
+					Level: 1.0,
+					Items: []*core.MarketItem{
+						{
+							CardPack: cardPacks["cardpack-magic"],
+							Price:    cardPackPrices["cardpack-magic"],
+						},
+					},
+				},
+			},
+		},
+	}
+	points[size.Index(2, 4)] = &core.OtherNationPoint{ // 機械の国
+		OtherNation: &core.OtherNation{
+			BaseNation: core.BaseNation{
+				NationID: "nation-mechanical",
+				Market: &core.Market{
+					Level: 1.0,
+					Items: []*core.MarketItem{
+						{
+							CardPack: cardPacks["cardpack-mechanical"],
+							Price:    cardPackPrices["cardpack-mechanical"],
+						},
+					},
+				},
+			},
+		},
+	}
+	points[size.Index(4, 2)] = &core.OtherNationPoint{ // お祭りの国
+		OtherNation: &core.OtherNation{
+			BaseNation: core.BaseNation{
+				NationID: "nation-carnival",
+				Market: &core.Market{
+					Level: 1.0,
+					Items: []*core.MarketItem{
+						{
+							CardPack: cardPacks["cardpack-fancy"],
+							Price:    cardPackPrices["cardpack-fancy"],
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for i := range points {
+		if points[i] != nil {
 			continue
 		}
+
+		x, y := size.XY(i)
 
 		if x == 4 && y == 4 {
 			points[i] = &core.BossPoint{
 				Boss:     &core.Enemy{Power: 30, BattleCardSlot: 10},
 				Defeated: false,
-			}
-			continue
-		}
-
-		if x%2 == 0 && y%2 == 0 {
-			points[i] = &core.OtherNationPoint{
-				OtherNation: &core.OtherNation{
-					BaseNation: core.BaseNation{
-						NationID: "other",
-						Market: &core.Market{
-							Level: 1.0,
-							Items: []*core.MarketItem{
-								{
-									CardPack: &core.CardPack{
-										Ratios: map[core.CardID]int{
-											"battle_card_1":    1,
-											"structure_card_1": 1,
-										},
-										NumPerOpen: 2,
-									},
-									Price: core.ResourceQuantity{Money: 3},
-								},
-							},
-						},
-					},
-				},
 			}
 			continue
 		}
